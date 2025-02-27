623eff84632fd4ec8550db44a55edf48ab470a7f
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-27 02:19:32 -0500

    params(chore): Add initial draft for centralizing common parameters

--- wiki/params.py
+++ wiki/params.py
@@ -5,14 +5,16 @@ Description: A class for storing parameters related to the wiki conversion proce
 
 import dataclasses
 import os
+import pathlib
+from typing import Literal
 
 
 @dataclasses.dataclass
 class WikiParameters:
     repo: str  # The repository path to clone, sync, or reference
     root: str  # The parent path for the current working directory
-    _type: str  # The type of conversion process, e.g. text, pdf, man
-    version: str  # The version of the docs, e.g. 2 or 3
+    conversion_type: Literal["text", "pdf", "man"]  # Type of conversion process
+    version: Literal["2", "3"]  # Version of the docs, e.g. 2 or 3
     verbose: bool  # Enable debug info
 
     def __post_init__(self):
@@ -23,3 +25,45 @@ class WikiParameters:
         # Default to local path if specified
         if self.root == ".":
             self.root = os.getcwd()
+
+        # Validate conversion type
+        if self.conversion_type not in {"text", "pdf", "man"}:
+            raise ValueError(f"Invalid conversion_type: {self.conversion_type}")
+
+        # Validate version
+        if self.version not in {"2", "3"}:
+            raise ValueError(f"Invalid version: {self.version}")
+
+    @property
+    def REPO_PATH(self) -> pathlib.Path:
+        return pathlib.Path(self.params.repo)
+
+    @property
+    def VERSION_PATH(self) -> list[pathlib.Path]:
+        version_map = {
+            "2": ["SDL2", "SDL2_image", "SDL2_mixer", "SDL2_net", "SDL2_ttf"],
+            "3": ["SDL3", "SDL3_image", "SDL3_mixer", "SDL3_net", "SDL3_ttf"],
+        }
+        return [self.REPO_PATH / v for v in version_map[self.params.version]]
+
+    @property
+    def ROOT_PATH(self) -> pathlib.Path:
+        return pathlib.Path(self._root)
+
+    @property
+    def TEXT_PATH(self) -> pathlib.Path:
+        text_dir = self.ROOT_PATH / "text"
+        text_dir.mkdir(parents=True, exist_ok=True)
+        return text_dir
+
+    @property
+    def PDF_PATH(self) -> pathlib.Path:
+        pdf_dir = self.ROOT_PATH / "pdf"
+        pdf_dir.mkdir(parents=True, exist_ok=True)
+        return pdf_dir
+
+    @property
+    def MAN_PATH(self) -> pathlib.Path:
+        man_dir = self.ROOT_PATH / "man"
+        man_dir.mkdir(parents=True, exist_ok=True)
+        return man_dir
