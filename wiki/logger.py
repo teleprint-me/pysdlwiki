aba85ac815be141dc8c473da8638c27ddd2bdcfd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-27 02:07:01 -0500

    logger(chore): Add thread-locking
    @note I don't expect this to be utilized at all, but I'm willing to be surprised

--- wiki/logger.py
+++ wiki/logger.py
@@ -7,30 +7,46 @@ Usage:
 class WikiBase:
     def __init__(self, verbose):
         self.logger = AutoLogger(self.__class__.__name__, verbose)
+
+NOTE: I think thread-locking here is overkill, but I'm willing to give it a shot.
+It might prove useful down the line and I'd rather have it be available than not.
+If it isn't used, it's trivial to remove. So no real loss here.
 """
 
 import logging
 import sys
+from threading import Lock
 
 
 class AutoLogger:
-    def __call__(self, cls_name: str, verbose: bool):
+    """
+    A callable class for automating the creation and configuration of loggers.
+    """
+
+    lock = Lock()
+
+    def __call__(self, cls_name: str, verbose: bool) -> logging.Logger:
         """
-        Initialize a Logger instance.
+        Initialize and return a Logger instance.
 
         :param cls_name: The name of the class that inherits from Logger.
         :param verbose: A boolean indicating whether to enable verbose logging.
+        :return: Configured logger instance.
         """
         _level = logging.DEBUG if verbose else logging.INFO
         _format = "%(levelname)s:%(filename)s:%(lineno)d: %(message)s"
         _logger = logging.getLogger(cls_name)
 
-        # Check if the logger has handlers to avoid adding duplicates
-        if not _logger.hasHandlers():
-            handler = logging.StreamHandler(stream=sys.stdout)
-            formatter = logging.Formatter(_format)
-            handler.setFormatter(formatter)
-            _logger.addHandler(handler)
-            _logger.setLevel(_level)
+        # Thread-safe check to avoid duplicate handlers
+        with self.lock:
+            if _logger.level != _level:
+                _logger.setLevel(_level)
+
+            # Check if the logger has handlers to avoid adding duplicates
+            if not _logger.hasHandlers():
+                handler = logging.StreamHandler(stream=sys.stdout)
+                formatter = logging.Formatter(_format)
+                handler.setFormatter(formatter)
+                _logger.addHandler(handler)
 
         return _logger
