f6b4e205e61dcc738e59895daa7dd6c361ca226b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-27 01:41:26 -0500

    logger(refactor): Convert the logger class into a factory to automate logger creation

--- wiki/logger.py
+++ wiki/logger.py
@@ -1,28 +1,36 @@
-# file: wiki/logger.py (1-21)
 """
 Copyright © 2023 Austin Berrio
 Module: wiki.logger
-Description: A simple parent class for child classes to inherit from.
+Description: A class for automating Logger configuration and instantiation.
 Usage:
-    class WikiBase(Logger):
-        def __init__(self, repo_path: str, type: str, version: int, verbose: bool):
-            super().__init__(self.__class__.__name__, verbose)
+
+class WikiBase:
+    def __init__(self, verbose):
+        self.logger = AutoLogger(self.__class__.__name__, verbose)
 """
 
 import logging
+import sys
 
 
-class WikiLogger:
-    def __init__(self, cls_name: str, verbose: bool):
+class AutoLogger:
+    def __call__(self, cls_name: str, verbose: bool):
         """
         Initialize a Logger instance.
 
         :param cls_name: The name of the class that inherits from Logger.
         :param verbose: A boolean indicating whether to enable verbose logging.
         """
-        self.level = logging.DEBUG if verbose else logging.INFO
-        logging.basicConfig(
-            level=self.level,
-            format="%(levelname)s:%(filename)s:%(lineno)d: %(message)s",
-        )
-        self.logger = logging.getLogger(cls_name)
+        _level = logging.DEBUG if verbose else logging.INFO
+        _format = "%(levelname)s:%(filename)s:%(lineno)d: %(message)s"
+        _logger = logging.getLogger(cls_name)
+
+        # Check if the logger has handlers to avoid adding duplicates
+        if not _logger.hasHandlers():
+            handler = logging.StreamHandler(stream=sys.stdout)
+            formatter = logging.Formatter(_format)
+            handler.setFormatter(formatter)
+            _logger.addHandler(handler)
+            _logger.setLevel(_level)
+
+        return _logger
