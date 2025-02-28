ced05baa2d03a9f871ce07035ac61360c265d4d9
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-28 01:35:11 -0500

    chore: Add simple install script for man pages

---
+++ install_man.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INSTALL_DIR="/usr/local/share/man/man3"
+SRC_DIR="output/man"
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+case "$1" in
+    install)
+        echo "Installing SDL Wiki Man pages..."
+        mkdir -pv "$INSTALL_DIR"
+        cp -v "$SRC_DIR"/SDL_*.1 "$INSTALL_DIR"
+        mandb
+        echo "Installation complete."
+        ;;
+    uninstall)
+        echo "Uninstalling SDL Wiki Man pages..."
+        rm -v "$INSTALL_DIR"/SDL_*.1
+        mandb
+        echo "Uninstallation complete."
+        ;;
+    *)
+        echo "Usage: $0 {install|uninstall}"
+        exit 1
+        ;;
+esac
