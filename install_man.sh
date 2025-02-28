d968a8a7a75e13ea575b842278d74cdaa66675c7
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-02-28 02:06:23 -0500

    install(patch): Add fix for handling man page references

--- install_man.sh
+++ install_man.sh
@@ -12,13 +12,13 @@ case "$1" in
     install)
         echo "Installing SDL Wiki Man pages..."
         mkdir -pv "$INSTALL_DIR"
-        cp -v "$SRC_DIR"/SDL_*.1 "$INSTALL_DIR"
+        cp -v "$SRC_DIR"/SDL_*.3.gz "$INSTALL_DIR"
         mandb
         echo "Installation complete."
         ;;
     uninstall)
         echo "Uninstalling SDL Wiki Man pages..."
-        rm -v "$INSTALL_DIR"/SDL_*.1
+        rm -v "$INSTALL_DIR"/SDL_*.3.gz
         mandb
         echo "Uninstallation complete."
         ;;
